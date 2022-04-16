6e6563bf4532afb355fe34f3d2ff70fdbdce3e1a
Amiran <chicobes1@gmail.com>
2022-04-16 20:42:45 +0300

    created markUp our app

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
+import './plugins';
 import '../css/style.css';
-import './plugins'
 import locations from './store/locations';
 
-locations.init()
-.then(res =>
-	{
-		console.log(res);
-		console.log(locations);
-		console.log(locations.getCitiesByCountryCode("PE"));
-	});
+locations.init().then(res => {
+  console.log(res);
+  console.log(locations);
+  console.log(locations.getCitiesByCountryCode('PE'));
+});
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -1,28 +1,26 @@
 import api from '../services/apiService';
 
-class Locations
-{
-	constructor(api)
-	{
-		this.api = api;
-		this.countries = null;
-		this.cities = null;
-	}
-	async init()
-	{
-		const response = await Promise.all([
-			this.api.cities(),
-			this.api.countries(),
-		])
-		const [cities , countries] = response;
-		this.countries = countries;
-		this.cities = cities;
-		return response
-	}
-	getCitiesByCountryCode(code)
-	{
-		return this.cities.filter(city => city.country_code === code)
-	}
+class Locations {
+  constructor(api) {
+    this.api = api;
+    this.countries = null;
+    this.cities = null;
+  }
+  async init() {
+    const response = await Promise.all([
+      this.api.countries(),
+      this.api.cities(),
+    ]);
+
+    const [countries, cities] = response;
+    this.countries = countries;
+    this.cities = cities;
+
+    return response;
+  }
+  getCitiesByCountryCode(code) {
+    return this.cities.filter(city => city.country_code === code);
+  }
 }
 
 const locations = new Locations(api);
